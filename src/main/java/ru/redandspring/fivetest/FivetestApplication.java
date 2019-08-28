3942c5974d6baee0732624860e871bc2fa874b42
Tretyakov <tretyakov.developer@gmail.com>
2019-08-28 20:39:13 +0300

    init

---
+++ src/main/java/ru/redandspring/fivetest/FivetestApplication.java
@@ -0,0 +1,13 @@
+package ru.redandspring.fivetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FivetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FivetestApplication.class, args);
+	}
+
+}
